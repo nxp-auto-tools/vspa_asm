36103bc594e54d9be877d50b16bb7b88df9bb2d1
Sergiu Crisan <[sergiu-dan.crisan@codeaurora.org]sergiu-dan.crisan@codeaurora.org>
2020-07-21 10:25:56 +0300

    CAF commit for lax assembler for Release alpha3.00.00.254 with target

---
+++ external/binutils-adl/gas/adl-asm-common.h
@@ -0,0 +1,10 @@
+#ifndef _ADL_ASM_COMMON_H_
+#define _ADL_ASM_COMMON_H_
+
+extern "C" int core;
+extern int init_core;
+
+void vspa_set_default_section();
+extern "C" int adl_size_lookup(unsigned);
+
+#endif
